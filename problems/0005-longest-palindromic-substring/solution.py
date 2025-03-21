b9b4e33636067f18d79cf7fe034548c360084739
Alikhan Sainov <40138758+asainov1@users.noreply.github.com>
2025-03-21 09:47:36 +0000

    [LeetCode Sync] Runtime - 4986 ms (14.12%), Memory - 18.1 MB (100.00%)

---
+++ problems/0005-longest-palindromic-substring/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        max_len = 1
+        max_str = s[0]
+        for i in range(len(s) - 1):
+            for j in range(i+1, len(s)):
+                if j-i+1 > max_len and s[i:j+1] == s[i:j+1][::-1]:
+                    max_len = j-i+1
+                    max_str = s[i:j+1]
+        return max_str            
