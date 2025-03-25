48e451484bb4ca8fa7a9eb2273f715ef997d53f6
Alikhan Sainov <40138758+asainov1@users.noreply.github.com>
2025-03-25 15:12:26 +0000

    [LeetCode Sync] Runtime - 3 ms (52.97%), Memory - 18.1 MB (100.00%)

--- problems/2048-build-array-from-permutation/solution.py
+++ problems/2048-build-array-from-permutation/solution.py
@@ -1,10 +1,6 @@
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
-        i = 0
-        j = len(nums) - 1
-
-        nums_2 = [0] * len(nums)
-        for i in range(len(nums)):
-            nums_2[i] = nums[nums[i]]
-        return nums_2
-
+        res = []
+        for num in nums:
+            res.append(nums[num])
+        return res
