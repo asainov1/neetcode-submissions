c780f9e00786511ef0568cea3fd3cf1d7cf3c2cd
Alikhan Sainov <40138758+asainov1@users.noreply.github.com>
2025-03-25 15:32:00 +0000

    [LeetCode Sync] Runtime - 57 ms (90.09%), Memory - 18.1 MB (99.99%)

--- problems/1580-shuffle-the-array/solution.py
+++ problems/1580-shuffle-the-array/solution.py
@@ -1,18 +1,10 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
-        i = 0
-        j = (len(nums) // 2) 
-        # nums_2 = [0] * len(nums)
-        # while i < (len(nums) // 2):
-        #     nums_2[2 * i], nums_2[2 * i + 1] = nums[i], nums[j]
-        #     # nums_2.append(nums[i])
-        #     # nums_2.append(nums[j])
-        #     i += 1
-        #     j += 1
-
-        nums_2 = []
-        for i in range(len(nums) // 2):
-            nums_2.append(nums[i])
-            nums_2.append(nums[j])
-            j += 1
-        return nums_2
+        res, l, r = [], 0, n
+        while l < (n): # a > b else a = b , a < b  ;  l > n l = n else l < n
+            res.append(nums[l])
+            l += 1
+            res.append(nums[r])
+            r += 1
+        print(l, r)
+        return res
