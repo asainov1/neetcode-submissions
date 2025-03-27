067610074e3806fbf774fad9c1879e0edfc3432c
Alikhan Sainov <40138758+asainov1@users.noreply.github.com>
2025-03-27 16:06:39 +0000

    [LeetCode Sync] Runtime - 133 ms (5.07%), Memory - 26.8 MB (99.95%)

--- problems/0121-best-time-to-buy-and-sell-stock/solution.py
+++ problems/0121-best-time-to-buy-and-sell-stock/solution.py
@@ -1,16 +1,15 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        i = 0
-        j = len(prices) - 1
-        max_num = max(prices)
-        ans = 0
-        mn = [0] * len(prices)
-        mn[0] = prices[0]
-        for i in range(1, len(prices)):
-            mn[i] = min(mn[i-1] , prices[i])
-        for i in range(1, len(prices)):
-            ans = max(ans, prices[i] - mn[i-1])
-        return ans
+        prefixmin = 10001
+        diff = 0
+        for i in range(len(prices)):
+            diff = max(diff, prices[i] - prefixmin)
+            prefixmin = min(prices[i], prefixmin)
+        return diff            
 
+
+
+                
+        
             
             
