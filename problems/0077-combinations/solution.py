7b18f550768b83c8c67c08ac4486961b4ead3de1
Alikhan Sainov <40138758+asainov1@users.noreply.github.com>
2025-03-31 15:40:45 +0000

    [LeetCode Sync] Runtime - 135 ms (19.67%), Memory - 59.6 MB (97.67%)

--- problems/0077-combinations/solution.py
+++ problems/0077-combinations/solution.py
@@ -1,13 +1,15 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        result = []
-        def backtrack (start , path):
-            if len(path) == k:
-                result.append(path[:])
-                return
-            for i in range(start, n + 1):
-                path.append(i)
-                backtrack(i + 1, path)
-                path.pop()
-        backtrack(1, [])
-        return result
+        res = []
+        sub = []
+        def recurs (crt, sub):
+            # ... 
+            if len(sub) == k:
+                res.append(sub[:])
+                return 
+            for i in range(crt, n + 1):
+                sub.append(i)
+                recurs(i + 1, sub)
+                sub.pop()   
+        recurs (1, [])
+        return res
