ab439b6a2af198b62115b74cf9e1697c47b71526
Alikhan Sainov <40138758+asainov1@users.noreply.github.com>
2025-10-01 16:45:49 +0000

    [LeetCode Sync] Runtime - 15 ms (72.51%), Memory - 14.3 MB (11.11%)

--- problems/0125-valid-palindrome/solution.py
+++ problems/0125-valid-palindrome/solution.py
@@ -1,9 +1,8 @@
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        import re
-        new_s = "".join([s_i.lower().strip(" ") for s_i in s])
-        new_s = re.sub(r'[^a-zA-Z0-9]', '', new_s)
-        reversed_s = new_s[::-1]
-        if new_s == reversed_s:
-            return True
-        return False
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        new_s = ''.join([i.lower() for i in s if i.isalnum()])
+        return new_s == new_s[::-1]
