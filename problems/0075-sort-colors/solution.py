ec9013cb9a685f48ca7fcfff319e56f6ec441121
Alikhan Sainov <40138758+asainov1@users.noreply.github.com>
2026-05-16 11:37:44 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.3 MB (60.77%)

--- problems/0075-sort-colors/solution.py
+++ problems/0075-sort-colors/solution.py
@@ -3,18 +3,16 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        left = 0
-        right = len(nums) - 1
-        i = 0
+        left, right, i =  0, len(nums) - 1, 0
+
         while i <= right:
             if nums[i] == 0:
-                nums[i], nums[left] = nums[left], nums[i]
+                nums[left], nums[i] = nums[i], nums[left]
                 left += 1
                 i += 1
             elif nums[i] == 2:
-                nums[i], nums[right] = nums[right], nums[i]
+                nums[right], nums[i] = nums[i], nums[right]
                 right -= 1
             else:
                 i += 1
-        
-        
+        return nums
