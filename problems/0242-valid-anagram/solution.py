4f8875b132f5cc7b021584aead56002fca18dd3b
Alikhan Sainov <40138758+asainov1@users.noreply.github.com>
2026-07-23 14:47:35 +0000

    [LeetCode Sync] Runtime - 11 ms (77.01%), Memory - 19.4 MB (45.65%)

--- problems/0242-valid-anagram/solution.py
+++ problems/0242-valid-anagram/solution.py
@@ -1,8 +1,15 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        return sorted(s) == sorted(t)
+from collections import Counter
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        counter = Counter(s)
+        counter_2 = Counter(t)
+        for num in t:
+            if num not in counter:
+                return False
+        if counter_2 != counter:
+            return False
+        return True
+
