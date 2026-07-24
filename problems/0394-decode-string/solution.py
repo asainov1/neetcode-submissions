6c003733ca985f2048fffdc65d10201e8ce4f787
Alikhan Sainov <40138758+asainov1@users.noreply.github.com>
2026-07-24 16:00:52 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.3 MB (77.35%)

--- problems/0394-decode-string/solution.py
+++ problems/0394-decode-string/solution.py
@@ -1,19 +1,24 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-        stack, curnum, curstring = [],0,''
-        for c in s:
-            if c == '[':
-                stack.append(curstring) # LIFO
-                stack.append(curnum)  # LIFO 
-                curstring = ''
-                curnum = 0
-            elif c == ']':
-                num = stack.pop()  # LIFO
-                prevstring = stack.pop() # LIFO
-                curstring = prevstring + num * curstring
-            elif c.isdigit():
-                curnum = curnum * 10 + int(c)
-            else:
-                curstring += c
-        return curstring
-
+        ans = ""
+        def backtrack(index):
+            tmp = ""
+            number = 0
+            while index < len(s):
+                if s[index].isalpha():
+                    tmp += s[index]
+                    index += 1
+         
+                elif s[index].isdigit():
+                    number = number * 10 + int(s[index])
+                    index += 1
+                elif s[index] == "[":
+                    decoded, index = backtrack(index + 1)
+                    tmp += number * decoded
+                    number = 0
+                    index += 1
+                else:
+                    return tmp, index
+            return tmp, index
+        ans, _ = backtrack(0)
+        return ans
