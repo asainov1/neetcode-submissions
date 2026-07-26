b9e32d4709cea7284f96069cda45fa6ce55fbafd
Alikhan Sainov <40138758+asainov1@users.noreply.github.com>
2026-07-26 08:22:48 +0000

    [LeetCode Sync] Runtime - 2 ms (21.45%), Memory - 19.5 MB (12.30%)

--- problems/0682-baseball-game/solution.py
+++ problems/0682-baseball-game/solution.py
@@ -1,16 +1,21 @@
+from collections import deque
 class Solution:
     def calPoints(self, operations: List[str]) -> int:
-        needed_dict = ["+","D","C"]
-        result = []
-        for op in range(len(operations)):
-            if operations[op] not in needed_dict:
-                result.append(int(operations[op]))
-            else:
-                if operations[op] == "C":
-                    result.pop()
-                elif operations[op] == "D":
-                    result.append(2 * int(result[-1])) 
-                else:
-                    result.append(int(result[-1]) + int(result[-2]))
-        print (result)
-        return sum(result)
+        q = deque()
+        for num in operations:
+            if num.isdigit():
+                q.append(num)
+            elif "-" in num:
+                q.append(int(num))
+            elif num == "C":
+                q.pop()
+            elif num == "D":
+                last_num = q.pop()
+                q.append(last_num)
+                new_num = 2 * int(last_num)
+                q.append(new_num)
+            elif num == "+":
+                q.append(int(q[-1]) + int(q[-2]))
+        print (q)
+        return sum([int(num) for num in q])
+
