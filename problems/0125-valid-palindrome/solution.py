3c0cdd0079c3f93c3d155305f010ba444e880d9a
Alikhan Sainov <40138758+asainov1@users.noreply.github.com>
2026-07-28 15:48:07 +0000

    [LeetCode Sync] Runtime - 39 ms (5.69%), Memory - 20.5 MB (22.55%)

--- problems/0125-valid-palindrome/solution.py
+++ problems/0125-valid-palindrome/solution.py
@@ -1,11 +1,39 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = s.lower().strip()
-        s = ''.join([c for c in s if c.isalnum()])
-        left, right = 0, len(s) - 1
-        while left < right:
-            if not s[left] == s[right]:
+        """
+        TC O(N) SC O(1)
+        right = 0
+        s = s.strip()
+        while left < len(s): 
+            # base case 
+            if s[left].lower() != s[right].lower():
                 return False
-            left += 1
-            right -= 1
+            elif s[left].isalnum():
+                left += 1
+            elif s[right].isalnum():
+                right -= 1
+        return True
+
+        x.isalnum()
+        s = "Aman,aplan,acanal:Panama"
+                         ^^
+
+        """
+        
+        left = 0
+        s = "".join(s.split())
+        right = len(s) - 1
+        print (s)
+        while left < right:
+            # base case 
+            if s[left].lower() != s[right].lower() and s[left].isalnum() and s[right].isalnum():
+                return False
+            elif not s[left].isalnum():
+                left += 1
+            elif not s[right].isalnum():
+                right -= 1
+            else:
+                left += 1
+                right -= 1
+                print (s[left], left, s[right], right)
         return True
